2c81f41f5f9a7a8a801dfa804d076be0c684403f
al-ameen36 <muhdabdullahi36@gmail.com>
2026-05-08 10:29:55 +0100

    feat: migrate to onnxruntime-rocm and execute on_statement_finalized synchronously

--- whisper_live/backend/base.py
+++ whisper_live/backend/base.py
@@ -322,7 +322,7 @@ class ServeClientBase(object):
                         logging.warning("Translation queue is full, skipping segment")
 
                 if self.on_statement_finalized:
-                    threading.Thread(target=self.on_statement_finalized, args=(completed_segment,), daemon=True).start()
+                    self.on_statement_finalized(completed_segment)
 
                 offset = min(duration, self.get_segment_end(s))
 
@@ -371,7 +371,7 @@ class ServeClientBase(object):
                             logging.warning("Translation queue is full, skipping segment")
 
                     if self.on_statement_finalized:
-                        threading.Thread(target=self.on_statement_finalized, args=(completed_segment,), daemon=True).start()
+                        self.on_statement_finalized(completed_segment)
 
             self.current_out = ''
             offset = min(duration, self.end_time_for_same_output)
